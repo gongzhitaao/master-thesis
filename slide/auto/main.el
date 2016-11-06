bc5ce98fba6a98fafe8f85ec2f2f1ca19b58d414
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-06 16:36:50 -0600

    slide init

---
+++ slide/auto/main.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor={usenames,dvipsnames}")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("microtype" "activate={true,nocompatibility}" "final" "tracking=true" "kerning=true" "spacing=nonfrench" "factor=1100" "stretch=10" "shrink=10") ("biblatex" "backend=biber" "style=authoryear-comp")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "geometry"
+    "microtype"
+    "minted"
+    "svg"
+    "biblatex")
+   (LaTeX-add-labels
+    "sec:org5383fac"
+    "sec:org7d5f25e"
+    "sec:orgad79406"
+    "sec:orgdcc52e0"
+    "sec:org4cca18c")
+   (LaTeX-add-bibliographies
+    "~/Documents/ms/thesis/thesis"))
+ :latex)
+
